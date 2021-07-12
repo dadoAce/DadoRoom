c8ceecb6b2da54912c59aa56078dd5d90871d1fb
Alfonso Acevedo <Eduardo_acevedo@grupoiikno.com>
2021-07-11 22:37:50 -0500

    refrencias update

--- Views/complementos/referencias/referencias.php
+++ Views/complementos/referencias/referencias.php
@@ -2,6 +2,6 @@
 <meta charset="UTF-8">
 <title>Principal</title>
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
-<link rel="stylesheet" href="<?= $this->base_url() ?>/Assets/local/bootstrap.min.css" >
+<link rel="stylesheet" href="/Assets/local/bootstrap.min.css" >
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-<link rel="stylesheet" href="<?= $this->base_url() ?>/Assets/dr_style/css/estilo.css"> 
+<link rel="stylesheet" href="/Assets/dr_style/css/estilo.css"> 
